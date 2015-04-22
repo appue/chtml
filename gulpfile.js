b82f9df4671ee2f8cf09d7df55dc0ae20733a759
xiong.shaole <slxiong@ctrip.com>
2015-04-22 18:59:32 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,23 @@ var gulp       = require('gulp'),
     watch      = require('gulp-watch'),
     livereload = require('gulp-livereload'),
     plumber    = require('gulp-plumber');
+
+var task = {
+    sass: function() {
+        // gulp.src('mockup/themes/*.scss')
+        //     .pipe(plumber())
+        //     .pipe(sass())
+        //     .pipe(minifycss())
+        //     .pipe(gulp.dest(buildPath +'themes'));
+        gulp.src('mockup/themes/*.scss')
+            .pipe(plumber())
+            .pipe(sass())
+            .pipe(gulp.dest('mockup/themes'));
+    }
+};
+
+gulp.task('default', function(){
+    task.sass();
+    task.watch();
+    task.connect();
+});
