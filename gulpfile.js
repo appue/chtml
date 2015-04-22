fc92e3470a0f021d9b0a484f891552344d005077
xiong.shaole <slxiong@ctrip.com>
2015-04-22 19:35:37 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 /*
+npm install yargs
 npm install --save-dev gulp
 npm install --save-dev gulp-sass
 npm install --save-dev gulp-minify-css
 npm install --save-dev gulp-connect
 npm install --save-dev gulp-watch
+npm install --save-dev gulp-shell
 npm install --save-dev gulp-livereload
 npm install --save-dev gulp-plumber
 npm install --save-dev gulp-batch
@@ -17,6 +19,7 @@ var gulp       = require('gulp'),
     sass       = require('gulp-sass'),
     minifycss  = require('gulp-minify-css'),
     connect    = require('gulp-connect'),
+    shell      = require('gulp-shell'),
     watch      = require('gulp-watch'),
     livereload = require('gulp-livereload'),
     plumber    = require('gulp-plumber');
@@ -32,9 +35,53 @@ var task = {
             .pipe(plumber())
             .pipe(sass())
             .pipe(gulp.dest('mockup/'));
-    }
+    },
+
+    watch: function(){
+        var self = this;
+
+        livereload.listen();
+
+        gulp.src('mockup/**/*.html')
+            .pipe(watch('mockup/**/*.html', function() {
+            }))
+            .pipe(livereload());
+
+        gulp.src('mockup/**/*.scss')
+            .pipe(watch('mockup/**/*.scss', function() {
+                self.sass();
+            }))
+            .pipe(livereload());
+    },
+
+    connect: function() {
+
+        var version = os.platform(),
+            url = '';
+
+        connect.server({
+            root: 'mockup',
+            port: '9999',
+            livereload: true
+        });
+
+        switch (version) {
+            case 'win32':
+                url = 'start http://localhost:9999';
+                break;
+            case 'darwin':
+                url = 'open http://localhost:9999';
+                break;
+        }
+
+        gulp.src('')
+            .pipe(shell(url));
+            
+    },
 };
 
 gulp.task('default', function(){
     task.sass();
+    task.watch();
+    task.connect();
 });
