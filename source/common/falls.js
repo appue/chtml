224c9caf0fbdaed4e53cfb535382c05f7668d81e
appue <277041948@qq.com>
2015-05-08 11:18:19 +0800

    modify

--- source/common/falls.js
+++ source/common/falls.js
@@ -9,13 +9,52 @@ angular.module('phoneApp')
         * }
         */
         init: function(params){
-            var $elem = document.querySelectorAll(params.elem +' li');
+            var el = document.querySelectorAll(params.elem +' li');
 
-            
-            var arrHeight
-            for (var i=0,len=$elem.length; i<len; i++) {
+            // var obj = {
+            //         'layout': [
+            //             {
+            //                 'z': //--索引值
+            //                 'h': //--高度
+            //                 'type': //--left or right
+            //                 'top': //--距离上面的高度
+            //             }
+            //         ],
+            //         'x': 0, //--左边总高度
+            //         'y': 0 //--右边总高度
+            //     };
 
+            var obj = {
+                'layout': [],
+                'x': 0, //--左边总高度
+                'y': 0 //--右边总高度
+            };
+
+            for (var i=0,len=el.length; i<len; i++) {
+                var h = el[i].offsetHeight;
+
+                if (obj.x <= obj.y) {
+                    obj.layout.push({
+                        'z': i,
+                        'h': h,
+                        'type': 'left',
+                        'top': obj.x + 10
+                    });
+                    obj.x = obj.layout[i].top + h;
+                } else {
+                    obj.layout.push({
+                        'z': i,
+                        'h': h,
+                        'type': 'right',
+                        'top': obj.y + 10
+                    });
+                    obj.y = obj.layout[i].top + h;
+                }
             }
+
+
+            return obj;
+
         }
     }
 
--- source/home/js/index.js
+++ source/home/js/index.js
@@ -14,9 +14,21 @@ angular.module('phoneApp')
        console.log(a.index('li'));
     }
 
-
-
-    SetFalls.init({
+    var obj = SetFalls.init({
         'elem': '.js_falls'
     }); 
+    
+    console.log(obj);
+
+    if (obj.x <= obj.y) {
+        document.querySelector('.js_falls').style.cssText = 'height:'+ obj.y +'px';
+    } else {
+        document.querySelector('.js_falls').style.cssText = 'height:'+ obj.x +'px';
+    }
+
+    var html = document.querySelectorAll('.js_falls li');
+
+    for(var i=0,len=html.length; i<len; i++) {
+        html[i].style.cssText = obj.layout[i].type +':0;top:'+ obj.layout[i].top +'px';
+    }
 });
