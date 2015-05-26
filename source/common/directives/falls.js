987555c0afbb645d7269c73141f78ecc0c11631a
appue <277041948@qq.com>
2015-05-26 12:12:11 +0800

    modify

---
+++ source/common/directives/falls.js
@@ -0,0 +1,13 @@
+angular.module('phoneApp')
+
+.directive('whenScrolled', function ($parse, $timeout) {
+    return function (scope, elm, attr) {
+        var raw = elm[0];
+
+        elm.bind('scroll', function () {
+            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                scope.$apply(attr.whenScrolled);
+            }
+        });
+    };
+});
--- source/forum/js/cate_list.js
+++ source/forum/js/cate_list.js
@@ -122,8 +122,8 @@ angular.module('phoneApp')
                         ],
                         SiteUrl: {
                             'url': [
-                                'home/#/index',
-                                'home/index.html#/index'
+                                'forum/#/thread-1.htm',
+                                'forum/index.html#/thread-1.htm'
                             ]
                         }
                     },
@@ -158,8 +158,80 @@ angular.module('phoneApp')
                         ],
                         SiteUrl: {
                             'url': [
-                                'home/#/index',
-                                'home/index.html#/index'
+                                'forum/#/thread-1.htm',
+                                'forum/index.html#/thread-1.htm'
+                            ]
+                        }
+                    },
+                    {
+                        ArticleId: 1,
+                        Images: [
+                            {
+                                ImageUrl: '../themes/temp/4.jpg',
+                                Description: '活动意图：《3-6岁儿童学习与发展指南》（以下简称《指南》）告诉我们，幼儿科学学习的方式是直接感知、亲身体验和实际操作，因此，让幼儿',
+                                Width: 200,
+                                Height: 321
+                            }
+                        ],
+                        Author: {
+                            UserId: 1,
+                            ImageUrl: '../themes/temp/3.jpg',
+                            UserName: '帖子发布者名称'
+                        },
+                        CategoryList: [
+                            {
+                                CateId: 1,
+                                CateName: '泥工'
+                            },
+                            {
+                                CateId: 1,
+                                CateName: '废旧材料'
+                            },
+                            {
+                                CateId: 1,
+                                CateName: '玩教具'
+                            }
+                        ],
+                        SiteUrl: {
+                            'url': [
+                                'forum/#/thread-1.htm',
+                                'forum/index.html#/thread-1.htm'
+                            ]
+                        }
+                    },
+                    {
+                        ArticleId: 1,
+                        Images: [
+                            {
+                                ImageUrl: '../themes/temp/5.jpg',
+                                Description: '活动意图：《3-6岁儿童学习与发展指南》（以下简称《指南》）告诉我们，幼儿科学学习的方式是直接感知、亲身体验和实际操作，因此，让幼儿',
+                                Width: 200,
+                                Height: 278
+                            }
+                        ],
+                        Author: {
+                            UserId: 1,
+                            ImageUrl: '../themes/temp/3.jpg',
+                            UserName: '帖子发布者名称'
+                        },
+                        CategoryList: [
+                            {
+                                CateId: 1,
+                                CateName: '泥工'
+                            },
+                            {
+                                CateId: 1,
+                                CateName: '废旧材料'
+                            },
+                            {
+                                CateId: 1,
+                                CateName: '玩教具'
+                            }
+                        ],
+                        SiteUrl: {
+                            'url': [
+                                'forum/#/thread-1.htm',
+                                'forum/index.html#/thread-1.htm'
                             ]
                         }
                     }
