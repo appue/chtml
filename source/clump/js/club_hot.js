432769507b21eee1036f2bb977be31465cc55ae5
appue <277041948@qq.com>
2015-06-07 21:51:55 +0800

    modify

--- source/clump/js/club_hot.js
+++ source/clump/js/club_hot.js
@@ -13,63 +13,40 @@ angular.module('phoneApp')
     widget
 ){
 
-    var pageView = {
-        init: function() {
-            var self = this;
-
-            self._setDeploy();
-
-            self._setHeader();
-
-            self._updateData();
-        },
-
-        _setDeploy: function () {
-            var self = this;
-
-            //--查看全部圈子
-            $scope.jumpClubList = {
-                'url': [
-                    'clump/#/club/list.htm',
-                    'clump/index.html#/club/list.htm'
-                ]
-            };
-        },
-
-        _setHeader: function() {
-            var self = this;
-            
-            //--设置返回按钮
-            $scope.backParam = {
-                'url': [
-                    'clump/#/find.htm',
-                    'clump/index.html#/find.htm'
-                ]
-            };
-        },
-
-        _updateData: function () {
-            var self = this;
-
-            $scope.addReport = function() {
-
-                widget.ajaxRequest({
-                    noMask: true,
-                    url: '$local/Tools/setReportArticle',
-                    data: {
-                        ArticleId: $stateParams.id, //-----帖子ID
-                        Contact: Contact,//-------联系方式
-                        ReportReason: ReportReason//--举报理由
-                    },
-                    success: function (data) {
-                        alert(data);
-                    }
-                });
-
-            };
+    //--查看全部圈子
+    $scope.redirectUrl = {
+        Club: {
+            'url': [
+                'clump/#/club/list.htm'
+            ]
         }
     };
 
-    pageView.init();
+    //--设置返回按钮
+    $scope.backParam = {
+        'url': [
+            'clump/#/find.htm',
+            'clump/index.html#/find.htm'
+        ]
+    };
+
+
+
+    $scope.addReport = function() {
+
+        widget.ajaxRequest({
+            noMask: true,
+            url: '$local/Tools/setReportArticle',
+            data: {
+                ArticleId: $stateParams.id, //-----帖子ID
+                Contact: Contact,//-------联系方式
+                ReportReason: ReportReason//--举报理由
+            },
+            success: function (data) {
+                alert(data);
+            }
+        });
+
+    };
 
 });
