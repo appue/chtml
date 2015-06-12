c38357f90b920b69ade6af9369840b815fdcbe02
appue <277041948@qq.com>
2015-06-12 15:10:20 +0800

    modify

--- source/home/js/index.js
+++ source/home/js/index.js
@@ -7,6 +7,7 @@ angular.module('phoneApp')
 .controller('HomeIndexCtrl', function (
     $scope, 
     $state,
+    $q,
     cachePool,
     routerRedirect,
     widget
@@ -44,32 +45,49 @@ angular.module('phoneApp')
 
     $scope.DataList = {
         ArticleList: [
-            {ArticleId: 1, Images: [{ImageUrl: '../themes/temp/1.jpg', Description: '所需材料：剪刀、彩纸、双面胶等等', Width: 200, Height: 162 } ], Author: {UserId: 1, ImageUrl: '../themes/temp/3.jpg', UserName: '帖子发布者名称'}, CategoryList: [{CateId: 1, CateName: '泥工'}, {CateId: 1, CateName: '废旧材料'}, {CateId: 1, CateName: '玩教具'} ], SiteUrl: {'url': ['forum/#/thread-1.htm?from='+currentUrl, 'forum/index.html#/thread-1.htm?from='+currentUrl] } },
-            {ArticleId: 1, Images: [{ImageUrl: '../themes/temp/2.jpg', Description: '活动意图：《3-6岁儿童学习与发展指南》（以下简称《指南》）告诉我们，幼儿科学学习的方式是直接感知、亲身体验和实际操作，因此，让幼儿', Width: 200, Height: 124 } ], Author: {UserId: 1, ImageUrl: '../themes/temp/3.jpg', UserName: '帖子发布者名称'}, CategoryList: [{CateId: 1, CateName: '泥工'}, {CateId: 1, CateName: '废旧材料'}, {CateId: 1, CateName: '玩教具'} ], SiteUrl: {'url': ['forum/#/thread-1.htm?from='+currentUrl, 'forum/index.html#/thread-1.htm?from='+currentUrl] } },
-            {ArticleId: 1, Images: [{ImageUrl: '../themes/temp/4.jpg', Description: '活动意图：《3-6岁儿童学习与发展指南》（以下简称《指南》）告诉我们，幼儿科学学习的方式是直接感知、亲身体验和实际操作，因此，让幼儿', Width: 200, Height: 321 } ], Author: {UserId: 1, ImageUrl: '../themes/temp/3.jpg', UserName: '帖子发布者名称'}, CategoryList: [{CateId: 1, CateName: '泥工'}, {CateId: 1, CateName: '废旧材料'}, {CateId: 1, CateName: '玩教具'} ], SiteUrl: {'url': ['forum/#/thread-1.htm?from='+currentUrl, 'forum/index.html#/thread-1.htm?from='+currentUrl] } },
-            {ArticleId: 1, Images: [{ImageUrl: '../themes/temp/5.jpg', Description: '活动意图：《3-6岁儿童学习与发展指南》（以下简称《指南》）告诉我们，幼儿科学学习的方式是直接感知、亲身体验和实际操作，因此，让幼儿', Width: 200, Height: 278 } ], Author: {UserId: 1, ImageUrl: '../themes/temp/3.jpg', UserName: '帖子发布者名称'}, CategoryList: [{CateId: 1, CateName: '泥工'}, {CateId: 1, CateName: '废旧材料'}, {CateId: 1, CateName: '玩教具'} ], SiteUrl: {'url': ['forum/#/thread-1.htm?from='+currentUrl, 'forum/index.html#/thread-1.htm?from='+currentUrl] } }
+            {ArticleId: 1, Images: [{ImageUrl: '../themes/temp/1.jpg', Description: '所需材料：剪刀、彩纸、双面胶等等', Width: 200, Height: 162 } ], Author: {UserId: 1, ImageUrl: '../themes/temp/3.jpg', UserName: '帖子发布者名称'}, CategoryList: [{CateId: 1, CateName: '泥工'}, {CateId: 1, CateName: '废旧材料'}, {CateId: 1, CateName: '玩教具'} ], SiteUrl: {'url': ['forum/#/thread-1.htm?from='+currentUrl, 'forum/index.html#/thread-1.htm?from='+currentUrl] } }
         ]
     };
 
 
-
+    //--获取幻灯片图片
     widget.ajaxRequest({
         noMask: true,
-        url: '$local/getHomeImage',
-        data: {
-        },
+        url: 'getHomeImage',
         success: function (data) {
-            
+             console.log('success');
+        },
+        error: function (data) {
+             console.log('error');
         }
     });
     
+    //--获取最新列表
     widget.ajaxRequest({
         noMask: true,
-        url: '$local/getHomeFollow',
+        url: 'getListArticle',
         data: {
+            CateId: 1,
+            PageIndex: 1, //---当前页码
+            PageSize: 6 //-----每页显示多少条记录
         },
         success: function (data) {
-            
+             console.log('success');
+        },
+        error: function (data) {
+             console.log('error');
         }
     });
+
+
+    // widget.ajaxRequest({
+    //     noMask: true,
+    //     url: 'getHomeFollow',
+    //     data: {
+    //     },
+    //     success: function (data) {
+            
+    //     }
+    // });
+
 });
