5016b76b3c115b76c38bcc4b3701122f1d86d744
enuobear <enuobear@gmail.com>
2015-06-14 01:14:20 +0800

    modify

--- source/clump/js/club_hot.js
+++ source/clump/js/club_hot.js
@@ -26,140 +26,24 @@ angular.module('phoneApp')
         'url': ['clump/#/find.htm']
     };
 
+    $scope.DataList = {};
 
     widget.ajaxRequest({
         noMask: true,
         url: 'getHotListClub',
         data: {
-            ShowNum: 20
+            ShowNum: 10
         },
         success: function (data) {
-            var res = {};
-
-            res.ClubList = data.ClubList || [];
-
-            angular.forEach(res.ClubList, function (v, k) {
+            angular.forEach(data.ClubList, function (v, k) {
                 v.SiteUrl = {
                     'url': ['/clump/#/club/detail-'+ v.ClubId +'.htm?from='+ currentUrl]
                 };
             });
-
-            $scope.DataList = res;
+            angular.extend($scope.DataList, data);
         },
         error: function (data) {
-
         }
     });
-
-
-
-    $scope.DataList = {
-        ClubList: [
-            {
-                ClubId: 1,
-                ClubName: '孩子们的手工王国',
-                ImageUrl: '../themes/temp/7.jpg',
-                TotalUser: 200,
-                CategoryList: [
-                    {CateName: '玩教具'},
-                    {CateName: '手工'}
-                ],
-                SiteUrl: {
-                    'url': ['/clump/#/club/detail-1.htm?from='+ currentUrl]
-                }
-            },
-            {
-                ClubId: 1,
-                ClubName: '孩子们的手工王国',
-                ImageUrl: '../themes/temp/7.jpg',
-                TotalUser: 200,
-                CategoryList: [
-                    {CateName: '玩教具'},
-                    {CateName: '手工'}
-                ],
-                SiteUrl: {
-                    'url': ['/clump/#/club/detail-1.htm?from='+ currentUrl]
-                }
-            },
-            {
-                ClubId: 1,
-                ClubName: '孩子们的手工王国',
-                ImageUrl: '../themes/temp/7.jpg',
-                TotalUser: 200,
-                CategoryList: [
-                    {CateName: '玩教具'},
-                    {CateName: '手工'}
-                ],
-                SiteUrl: {
-                    'url': ['/clump/#/club/detail-1.htm?from='+ currentUrl]
-                }
-            },
-            {
-                ClubId: 1,
-                ClubName: '孩子们的手工王国',
-                ImageUrl: '../themes/temp/7.jpg',
-                TotalUser: 200,
-                CategoryList: [
-                    {CateName: '玩教具'},
-                    {CateName: '手工'}
-                ],
-                SiteUrl: {
-                    'url': ['/clump/#/club/detail-1.htm?from='+ currentUrl]
-                }
-            },
-            {
-                ClubId: 1,
-                ClubName: '孩子们的手工王国',
-                ImageUrl: '../themes/temp/7.jpg',
-                TotalUser: 200,
-                CategoryList: [
-                    {CateName: '玩教具'},
-                    {CateName: '手工'}
-                ],
-                SiteUrl: {
-                    'url': ['/clump/#/club/detail-1.htm?from='+ currentUrl]
-                }
-            },
-            {
-                ClubId: 1,
-                ClubName: '孩子们的手工王国',
-                ImageUrl: '../themes/temp/7.jpg',
-                TotalUser: 200,
-                CategoryList: [
-                    {CateName: '玩教具'},
-                    {CateName: '手工'}
-                ],
-                SiteUrl: {
-                    'url': ['/clump/#/club/detail-1.htm?from='+ currentUrl]
-                }
-            },
-            {
-                ClubId: 1,
-                ClubName: '孩子们的手工王国',
-                ImageUrl: '../themes/temp/7.jpg',
-                TotalUser: 200,
-                CategoryList: [
-                    {CateName: '玩教具'},
-                    {CateName: '手工'}
-                ],
-                SiteUrl: {
-                    'url': ['/clump/#/club/detail-1.htm?from='+ currentUrl]
-                }
-            },
-            {
-                ClubId: 1,
-                ClubName: '孩子们的手工王国',
-                ImageUrl: '../themes/temp/7.jpg',
-                TotalUser: 200,
-                CategoryList: [
-                    {CateName: '玩教具'},
-                    {CateName: '手工'}
-                ],
-                SiteUrl: {
-                    'url': ['/clump/#/club/detail-1.htm?from='+ currentUrl]
-                }
-            }
-        ]
-    };
 
 });
--- source/clump/js/club_list.js
+++ source/clump/js/club_list.js
@@ -27,15 +27,6 @@ angular.module('phoneApp')
             var res = {ClubList: []};
 
             angular.forEach(data.ClubList, function (v, k) {
-
-                //--ToDo
-                if (k == 0 || k == 1 || k==2) {
-                    v.Letter = "A";
-                } else {
-                    v.Letter = "D";
-                }
-
-
                 v.SiteUrl = {
                     'url': ['clump/#/club/detail-'+ v.ClubId +'.htm']
                 };
@@ -43,7 +34,7 @@ angular.module('phoneApp')
                 if (res.ClubList.length == 0) {
 
                     res.ClubList.push({
-                        'Letter': 'A',
+                        'Letter': v.Letter,
                         'List': [v]
                     });
 
@@ -69,56 +60,5 @@ angular.module('phoneApp')
 
         }
     });
-
-
-
-
-    // var datas = {
-    //     ClubList: [
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/1.jpg', TotalUser: '258', Letter: 'A', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/2.jpg', TotalUser: '258', Letter: 'A', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/3.jpg', TotalUser: '258', Letter: 'B', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/4.jpg', TotalUser: '258', Letter: 'B', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/5.jpg', TotalUser: '258', Letter: 'C', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/6.jpg', TotalUser: '258', Letter: 'C', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/7.jpg', TotalUser: '258', Letter: 'D', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/8.jpg', TotalUser: '258', Letter: 'D', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/9.jpg', TotalUser: '258', Letter: 'F', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/1.jpg', TotalUser: '258', Letter: 'F', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/3.jpg', TotalUser: '258', Letter: 'G', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/2.jpg', TotalUser: '258', Letter: 'G', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/4.jpg', TotalUser: '258', Letter: 'G', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/5.jpg', TotalUser: '258', Letter: 'H', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/6.jpg', TotalUser: '258', Letter: 'H', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/7.jpg', TotalUser: '258', Letter: 'H', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/8.jpg', TotalUser: '258', Letter: 'H', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } },
-    //         {ClubId: 1, ClubName: '孩子们的营养餐', ImageUrl: '../themes/temp/9.jpg', TotalUser: '258', Letter: 'H', CategoryList: [{CateId: 1, CateName: '玩教具'}, {CateId: 1, CateName: '手工'} ], SiteUrl: {'url': ['clump/#/club/detail-1.htm', 'clump/index.html#/club/detail-1.htm'] } }
-    //     ]
-    // };
-
-    // $scope.DataList = [];
-
-    // angular.forEach(datas.ClubList, function (v, k) {
-    //     if ($scope.DataList.length == 0) {
-
-    //         $scope.DataList = [{
-    //             'Letter': 'A',
-    //             'List': [v]
-    //         }];
-
-    //     } else {
-
-    //         var len = $scope.DataList.length;
-
-    //         if ($scope.DataList[len-1].Letter == v.Letter) {
-    //             $scope.DataList[len-1].List.push(v);
-    //         } else {
-    //             $scope.DataList.push({
-    //                 'Letter': v.Letter,
-    //                 'List': [v]
-    //             });
-    //         }
-    //     }
-    // });
 
 });
