892af2f20b65a6fa395c10f2f1b42226c319affa
enuobear <enuobear@gmail.com>
2015-06-18 19:11:47 +0800

    modify

--- source/forum/js/photo_title.js
+++ source/forum/js/photo_title.js
@@ -13,11 +13,50 @@ angular.module('phoneApp')
     widget
 ){
 
+    $scope.DataList = {};
+
     //--设置返回按钮
     $scope.backParam = {
         'url': ['forum/#/photo/edit.htm']
     };
-    $scope.nextPage = {
-        'url': ['forum/#/photo/cate-0.htm']
+    $scope.nextPage = function () {
+        var empty = false;
+        angular.forEach($scope.DataList.ImagesList, function (v, k) {
+            if (!v.Content) {
+                empty = true;
+                return;
+            }
+        });
+
+        if (empty) {
+            widget.msgToast('请填您想说的话哦！');
+        } else {
+            routerRedirect.toJump({
+                'url': ['forum/#/photo/cate-0.htm']
+            });
+        }
+    };
+
+    $scope.DataList.ImagesList = [
+        {
+            ImageUrl: '../themes/temp/7.jpg',
+            Content: ''
+        },
+        {
+            ImageUrl: '../themes/temp/7.jpg',
+            Content: ''
+        },
+        {
+            ImageUrl: '../themes/temp/7.jpg',
+            Content: ''
+        },
+        {
+            ImageUrl: '../themes/temp/7.jpg',
+            Content: ''
+        }
+    ];
+
+    $scope.dataDelete = function (key) {
+        $scope.DataList.ImagesList.splice(key, 1);
     };
 });
