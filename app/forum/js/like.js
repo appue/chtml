8f727dd5f92a08094d37f6f6b4a89f8524c583c3
appue <277041948@qq.com>
2015-06-29 13:53:41 +0800

    modify

--- app/forum/js/like.js
+++ app/forum/js/like.js
@@ -1,56 +1,22 @@
-/*
-* 发现-猜你喜欢
-* /clump/#/like.html
-*/
+//发现-猜你喜欢
 angular.module('phoneApp')
 
 .controller('tFindLike', function (
-    $scope, 
-    $state, 
-    $stateParams, 
-    $location,
+    $scope,
     $timeout,
-    widget,
-    ENV
+    widget
 ){
     //--设置返回按钮
-    $scope.backParam = { 'route': '#/forum/clump/find.htm' };
+    // $scope.backParam = { 'route': '#/forum/clump/find.htm' };
 
-    $scope.Deploy = {
-        pageIndex: 0,
-        pageSize: 5,
-        isLoading: false
-    };
-
-
-    $scope.DataList = {
-        ArticleList: []
-    };
+    $scope.DataList = {};
 
     widget.ajaxRequest({
         noMask: true,
         url: 'getFindLike',
         data: {},
         success: function (data) {
-
-            // angular.forEach(data.PhotoList, function (v, k) {
-            //     if (ENV.isHybrid) {
-            //         v.SiteUrl = {
-            //             'url': [v.AppUrl]
-            //         };
-            //     } else {
-            //         v.SiteUrl = {
-            //             'url': [v.H5Url]
-            //         };
-            //     }
-            // });
-
-            angular.forEach(data.ArticleList, function (v, k) {
-                v.SiteUrl = '#/forum/thread-'+ v.ArticleId +'.htm';
-
-                // $scope.DataList.ArticleList.push(v);
-                angular.extend($scope.DataList, data);
-            });
+            angular.extend($scope.DataList, data);
 
             $timeout($scope.setFalls, 0);
         }
