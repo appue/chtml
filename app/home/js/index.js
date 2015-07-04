618faba9510edf742e6f32209a56d7427ff9b8dd
enuobear <enuobear@gmail.com>
2015-07-04 19:50:14 +0800

    modify

--- app/home/js/index.js
+++ app/home/js/index.js
@@ -15,25 +15,39 @@ angular.module('phoneApp')
     cachePool,
     widget
 ){
-
-    $scope.footerTab = 1;
-
-    $scope.Deploy = {
-        currentTab: 1,
-        pageIndex: 0,
-        pageSize: 5,
-        isMore: true,
-        isLoading: false
-    };
-    $scope.DataList = {
-        ListLeft: [], 
-        ListRight: []
-    };
+    var initConfig = {
+        init: function  () {
+            var self = this;
     
-    //显示loadding
-    $ionicLoading.show({
-        template: 'Loading...'
-    });
+            //显示loadding
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+
+            $scope.footerTab = 1;
+            $scope.DataList = {};
+
+            $scope.Deploy = {
+                currentTab: 1
+            };
+
+            self.deploy();
+        },
+
+        deploy: function () {
+            angular.extend($scope.Deploy, {
+                pageIndex: 0,
+                pageSize: 5,
+                isMore: true,
+                isLoading: false
+            });
+
+            $scope.DataList.ListLeft = [];
+            $scope.DataList.ListRight = [];
+        }
+    };
+
+    initConfig.init();
 
 
     //$ionicBackdrop.retain();
@@ -53,50 +67,11 @@ angular.module('phoneApp')
     });
 
 
-    $scope.Deploy.showLeft = function () {
-        $scope.Deploy.currentTab = 1;
-        // $scope.pageIndex = 0;
-
-        if ($scope.Deploy.pageIndexLeft * $scope.Deploy.pageSize >= $scope.Deploy.pageTotalLeft) {
-            $scope.Deploy.isLoading = true;
-
-            $timeout(function () {
-                $scope.setFalls('.mod_list_falls');
-            }, 0);
-            return;
-        }
-                
-        $scope.Deploy.isLoading = false;
+    $scope.$watch('Deploy.currentTab', function () {
+        initConfig.deploy();
 
         $scope.loadMore();
-    };
-
-    $scope.Deploy.showRight = function () {
-        if (cachePool.pull('UserInfo')) {
-
-            $scope.Deploy.currentTab = 2;
-            // $scope.pageIndex = 0;
-
-            if ($scope.Deploy.pageIndexRight * $scope.Deploy.pageSize >= $scope.Deploy.pageTotalRight) {
-                $scope.Deploy.isLoading = true;
-                $timeout(function () {
-                    $scope.setFalls('.mod_list_falls');
-                }, 0);
-                return;
-            }
-
-            $scope.Deploy.isLoading = false;
-
-            $scope.loadMore();
-
-        } else {
-
-            // routerRedirect.toJump({
-            //     url: ['entry/#/login.htm?from='+ currentUrl]
-            // });
-
-        }
-    };
+    });
 
     $scope.loadMore = function () {
 
@@ -105,13 +80,12 @@ angular.module('phoneApp')
             $scope.Deploy.isLoading = true;
 
             if ($scope.Deploy.currentTab == 1) {
-                $scope.Deploy.pageIndexLeft++;
-                $scope.Deploy.pageIndex = $scope.Deploy.pageIndexLeft;
+                $scope.Deploy.pageIndex++;
 
-                // if ($scope.Deploy.pageTotalLeft && ($scope.Deploy.pageIndexLeft * $scope.Deploy.pageSize - $scope.Deploy.pageTotalLeft)>$scope.Deploy.pageSize) {
-                //     $scope.Deploy.isMore = false;
-                //     return;
-                // }
+                if ($scope.Deploy.pageTotal && ($scope.Deploy.pageIndex * $scope.Deploy.pageSize - $scope.Deploy.pageTotal)>$scope.Deploy.pageSize) {
+                    $scope.Deploy.isMore = false;
+                    return;
+                }
 
                 //--获取最新列表
                 widget.ajaxRequest({
@@ -123,8 +97,7 @@ angular.module('phoneApp')
                         PageSize: $scope.Deploy.pageSize
                     },
                     success: function (data) {
-                        $scope.Deploy.pageTotalLeft = data.Total || 0;
-                        $scope.Deploy.pageTotal = $scope.Deploy.pageTotalLeft;
+                        $scope.Deploy.pageTotal = data.Total || 0;
 
                         $scope.DataList.ListLeft = $scope.DataList.ListLeft.concat(data.ArticleList);
 
@@ -141,10 +114,9 @@ angular.module('phoneApp')
                 });
 
             } else {
-                $scope.Deploy.pageIndexRight++;
-                $scope.Deploy.pageIndex = $scope.Deploy.pageIndexRight;
+                $scope.Deploy.pageIndex++;
 
-                if ($scope.Deploy.pageTotalRight && ($scope.Deploy.pageIndexRight * $scope.Deploy.pageSize - $scope.Deploy.pageTotalRight)>$scope.Deploy.pageSize) {
+                if ($scope.Deploy.pageTotal && ($scope.Deploy.pageIndex * $scope.Deploy.pageSize - $scope.Deploy.pageTotal)>$scope.Deploy.pageSize) {
                     $scope.Deploy.isMore = false;
                     return;
                 }
@@ -158,8 +130,7 @@ angular.module('phoneApp')
                         PageSize: $scope.Deploy.pageSize
                     },
                     success: function (data) {
-                        $scope.Deploy.pageTotalRight = data.Total || 0;
-                        $scope.Deploy.pageTotal = $scope.Deploy.pageTotalRight;
+                        $scope.Deploy.pageTotal = data.Total || 0;
 
                         $scope.DataList.ListRight = $scope.DataList.ListRight.concat(data.ArticleList);
 
@@ -167,7 +138,7 @@ angular.module('phoneApp')
 
                         $scope.Deploy.isLoading = false;
 
-                        // $scope.$broadcast('scroll.infiniteScrollComplete');
+                        $scope.$broadcast('scroll.infiniteScrollComplete');
                     },
                     error: function (data) {
                         $scope.Deploy.isLoading = false;
