862f5230f4fcf10fd3ee5f88d2a91e25c2aff778
enuobear <enuobear@gmail.com>
2015-07-06 22:35:51 +0800

    modify

---
+++ app/forum/js/user_index.js
@@ -0,0 +1,45 @@
+angular.module('phoneApp')
+.controller('tUserIndex', function (
+	$scope,
+	$stateParams
+) {
+
+	console.log($stateParams.id);
+
+	$scope.footerTab = 5; //--底部tab初始化高亮
+
+	$scope.goDataSet = function () { //--主人态设置页，客人态查看资料页
+
+		var url = true ? '/set.htm' : '/data.htm';
+
+		routerRedirect.toJump({
+			'url': [
+				'member/#/personal-' + $stateParams.id + url
+			]
+		});
+
+	};
+
+	$scope.redirectUrl = {
+		goAttention: { //--去关注页
+			'url': ['member/#/personal-' + $stateParams.id + '/attention.htm']
+		},
+
+		goFans: { //--去粉丝页
+			'url': ['member/#/personal-' + $stateParams.id + '/fans.htm']
+		},
+
+		goLevel: { //--去用户等级页
+			'url': ['member/#/personal-' + $stateParams.id + '/level.htm']
+		},
+
+		goIssue: { //--去发布页
+			'url': ['member/#/personal-' + $stateParams.id + '/issue.htm']
+		}
+	};
+
+	$scope.DataList = {
+		ClubList: [
+		]
+	}
+});
