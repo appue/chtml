4e8aedaac8a896d8b262150940d66f44f8fd607a
enuobear <enuobear@gmail.com>
2015-07-11 16:06:06 +0800

    modify

--- app/common/directives/router_redirect.js
+++ app/common/directives/router_redirect.js
@@ -1,4 +1,4 @@
-angular.module('phoneApp')
+router_angular.module('phoneApp')
 
 .directive('pageBack', function (
     $window,
@@ -34,6 +34,32 @@ angular.module('phoneApp')
                 // $ionicHistory.goBack();
 
                 // $rootScope.$ionicGoBack();
+            });
+        }
+    };
+})
+
+
+.directive('pageJump', function (
+    $window,
+    $state,
+    $rootScope,
+    $stateParams,
+    $ionicHistory,
+    $ionicViewSwitcher
+) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+
+            element.on('click', function (event) {
+                
+                var router = attrs.router,
+                    options = attrs.options ? JSON.parse(attrs.options) : {};
+
+                $ionicViewSwitcher.nextDirection('forward');
+                $state.go(router, options);
+
             });
         }
     };
