8c9d7cdc8a05c08363ec81a598da641be86113fa
enuobear <enuobear@gmail.com>
2015-07-15 23:57:34 +0800

    modify

--- app/user/js/feedback.js
+++ app/user/js/feedback.js
@@ -3,8 +3,36 @@ angular.module('phoneApp')
 
 .controller('tUserFeedback', function(
     $scope, 
-    $state
+    $state,
+    widget
 ){
+    $scope.cInput = {};
 
-    
+    $scope.submitData = function () {
+
+        if (!$scope.cInput.content) {
+            widget.msgToast('请输入您需要反馈的信息！');
+            return;
+        }
+
+        if (!$scope.cInput.phone) {
+            widget.msgToast('请输入您的联系方式！');
+            return;
+        }
+
+
+        widget.ajaxRequest({
+            url: 'setFeedback',
+            data: {
+                Content: $scope.cInput.content,
+                Contact: $scope.cInput.contact
+            },
+            success: function (data) {
+                if (data.Response && data.Response.Ack=="success") {
+                    widget.msgToast('请输入您的联系方式！');  
+                }
+            }
+        });
+    };
+
 });
