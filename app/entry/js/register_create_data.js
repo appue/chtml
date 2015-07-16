c0ca72ba0e2b36530573e8733e04dd4daae52a8d
enuobear <enuobear@gmail.com>
2015-07-16 09:35:40 +0800

    modify

--- app/entry/js/register_create_data.js
+++ app/entry/js/register_create_data.js
@@ -1,30 +1,38 @@
 angular.module('phoneApp')
-    .controller('registerCreateCtrl', function ($scope, $state, $ionicPopup, widget) {
 
-        $scope.inputVal = widget.cacheData('personalData') || { //数据初始化
-            sex: 1,
-            job: '园长',
-            city: {
-                Id: 2,
-                Name: '北京'
-            },
-            village: {
-                Id: 23,
-                Name: '朝阳区'
-            }
-        };
+.controller('registerCreateCtrl', function (
+    $scope,
+    $state,
+    $ionicPopup,
+    $ionicViewSwitcher,
+    widget
+) {
 
-        $scope.goNextStep = function () { //下一步按钮
+    $scope.inputVal = widget.cacheData('personalData') || { //数据初始化
+        sex: 1,
+        job: '园长',
+        city: {
+            Id: 2,
+            Name: '北京'
+        },
+        village: {
+            Id: 23,
+            Name: '朝阳区'
+        }
+    };
 
-            if (!$scope.inputVal.nickname) {
-                widget.msgToast('请输入昵称');
-                return;
-            }
+    $scope.goNextStep = function () { //下一步按钮
 
-            widget.cacheData('personalData', $scope.inputVal);
+        if (!$scope.inputVal.nickname) {
+            widget.msgToast('请输入昵称');
+            return;
+        }
 
-            $state.go('forum.registerAccount');
+        widget.cacheData('personalData', $scope.inputVal);
 
-        };
+        $ionicViewSwitcher.nextDirection("forward");
+        $state.go('forum.registerAccount');
 
-    });
+    };
+
+});
