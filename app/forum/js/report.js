4c236a219c5f32c5f583eaaf18a3eed148c2a3e8
enuobear <enuobear@gmail.com>
2015-08-17 11:52:23 +0800

    modify

--- app/forum/js/report.js
+++ app/forum/js/report.js
@@ -8,21 +8,16 @@ angular.module('phoneApp')
     widget
 ){
 
-    //--设置返回按钮
-    // $scope.backParam = {
-    //     'url': ['forum/#/thread-'+ $stateParams.id +'.htm']
-    // };
-
-    $scope.inputData = {};
+    $scope.cInput = {};
     
     $scope.submitData = function() {
 
-        if (!$scope.inputData.contact) {
+        if (!$scope.cInput.contact) {
             widget.msgToast('请填您的联系方式！');
             return;
         }
 
-        if (!$scope.inputData.content) {
+        if (!$scope.cInput.content) {
             widget.msgToast('请填上举报理由！');
             return;
         }
@@ -33,8 +28,8 @@ angular.module('phoneApp')
             url: 'setReportArticle',
             data: {
                 ArticleId: $stateParams.id, //-----帖子ID
-                Contact: $scope.inputData.contact,//-------联系方式
-                ReportReason: $scope.inputData.content //--举报理由
+                Contact: $scope.cInput.contact,//-------联系方式
+                ReportReason: $scope.cInput.content //--举报理由
             },
             success: function (data) {
                 widget.msgToast('感谢您的举报！');
