8ab1e4f14a75d81adccd9719ec756e7eebaf6c39
appue <appue@github.com>
2015-10-06 15:52:05 +0800

    modify

--- app/code/js/club.js
+++ app/code/js/club.js
@@ -7,4 +7,22 @@ angular.module('Tjoys')
 ){
     $rootScope.Menu = 'club';
     $rootScope.SubMenu = 'club';
+
+    $scope.DataList = {};
+
+    $scope.loadMore = function () {
+        widget.ajaxRequest({
+            scope: $scope,
+            url: 'getClubList',
+            data: {},
+            success: function (res) {
+                $scope.DataList = res;
+            },
+            error: function (err) {
+                widget.msgToast('失败');
+            }
+        });
+    };
+
+    $scope.loadMore();
 });
